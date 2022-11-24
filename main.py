61952064263b20c79480b9993d4c762357a3a38b
msg8925 <msg.8925@gmail.com>
2022-11-24 19:17:32 +0700

    Log in system successfully working

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from db_funcs import open_db, insert_into_db, select_from_db
 from models import Employee
+import bcrypt
 
 DB_NAME="company.db"
 
 open_db(DB_NAME)
 
-employee = Employee("John", "Smith", "user1", "1234", 20000)
+passwd = "1234"
+hashed_passwd = bcrypt.hashpw(passwd.encode(), bcrypt.gensalt())
+print(hashed_passwd)
+
+employee = Employee("John", "Smith", "user1", hashed_passwd, 20000)
 insert_into_db(DB_NAME, employee)
 
 username = input("Please enter your username: ")
 password = input("Please enter your password: ") 
-
+#hashed_password = bcrypt.hashpw(password.encode())
+#print(hashed_password)
+# , 
 
 user = select_from_db(DB_NAME, username)
 #print(user[4])
 
-if user[4] == password:
+if bcrypt.checkpw(password.encode(), user[4]):
     print("Successfully logged in.")
 else:
     print("Incorrect login credentials.")
