e9ead22a1b70ddb4fc68110b8ee1fc5c6ea991ef
msg8925 <msg.8925@gmail.com>
2022-11-25 14:42:44 +0700

    Login required decorator implemented successfully

---
+++ decorators.py
@@ -0,0 +1,34 @@
+from auth import login, register
+
+def login_required(original_function):
+    
+    # This code is run before original functions
+    def wrapper_function():
+       
+        result = login()
+        
+        # No account found
+        if result == 1:
+            print("Account does not exist. Create one now: ")
+            if register() == 0:
+                return original_function()
+            else:
+                return 3    
+
+        # Incorrect credentials
+        elif result == 2:
+            return 2
+        
+        # If login successful
+        elif result == 0:
+            return original_function()
+
+        # Any other result
+        else:
+            print(f"Result: {result}")
+            return 4
+
+    return wrapper_function
+
+
+
--- main.py
+++ main.py
@@ -1,92 +1,14 @@
 from db_funcs import open_db, insert_into_db, select_from_db
-from models import Employee
-import bcrypt
+# from auth import login, register 
+from decorators import login_required
 
 DB_NAME="company.db"
 
 open_db(DB_NAME)
 
-
-def login():
-    
-    username = input("Please enter your username: ")
-    password = input("Please enter your password: ") 
-
-    user = select_from_db(DB_NAME, username)
-    if user == None:
-        #print(user[4])
-        print("No account with username provided.")
-        return 1
-    
-    else:
-        if bcrypt.checkpw(password.encode(), user[4]):
-            print("Successfully logged in.")
-            return 0
-        
-        else:
-            print("Incorrect login credentials.")
-            return 2
+@login_required
+def some_secure_function():
+    print("This function will run and shows results after the decorator has run.")
 
 
-def register():
-
-    firstname = input("Please enter your firstname: ")
-    lastname = input("Please enter your lastname: ")
-    username = input("Please enter your username: ")
-    password_1 = input("Please enter your password: ")
-    password_2 = input("Please enter your password again: ")
-
-    # Check username does not already exist
-    user = select_from_db(DB_NAME, username)
-    # If there is no user with that name
-    if user:
-        print("This username already exists.")
-        return 1
-
-    # Check the passwords match
-    else: 
-        if password_1 != password_2:
-            print("Passwords do not match.")
-            return 2
-
-        else:
-            hashed_password = bcrypt.hashpw(password_1.encode(), bcrypt.gensalt())
-            #print(hashed_password)
-
-            employee = Employee(firstname, lastname, username, hashed_password, 20000)
-            insert_into_db(DB_NAME, employee)
-    
-            return 0
-
-
-# passwd = "abcd"
-# hashed_passwd = bcrypt.hashpw(passwd.encode(), bcrypt.gensalt())
-# print(hashed_passwd)
-
-# employee = Employee("Jane", "Doe", "user2", hashed_passwd, 20000)
-# insert_into_db(DB_NAME, employee)
-
-
-
-
-result = login()
-if result == 1:
-    print("Account does not exist: Create one now: ")
-    register()
-else:
-    print(f"Result: {result}")
-    
-
-# user = select_from_db(DB_NAME, username)
-# #print(user[4])
-
-# if bcrypt.checkpw(password.encode(), user[4]):
-#     print("Successfully logged in.")
-# else:
-#     print("Incorrect login credentials.")
-
-
-
-
-
-    
+some_secure_function()
